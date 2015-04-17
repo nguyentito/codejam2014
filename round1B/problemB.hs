b22bc0f907b93f1147a4b20838e9ffb1f43f2822
Nguyễn Lê Thành Dũng <nltd@nguyentito.eu>
2015-04-18 01:56:23 +0200

    Automata-based solution for Round 1B problem B (following gnomnain's idea)

--- round1B/problemB.hs
+++ round1B/problemB.hs
@@ -1,10 +1,10 @@
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BangPatterns, LambdaCase #-}
 
 import Control.Applicative
 import Control.Monad
-import Data.Word
 import Data.Bits
-import qualified Data.Set as Set
+import qualified Data.Map as Map
+import Data.Word
 import System.IO
 
 main = do
@@ -19,6 +19,37 @@ parse (numCasesStr:rest) = map parse' $ take numCases rest
                       (read sa, read sb, read sk)
 
 -- Very naive solution
-solve :: (Word32, Word32, Word32) -> Int
-solve (a, b, k) = length [ () | x <- [0..(a-1)], y <- [0..(b-1)], x .&. y < k]
+solveNaive :: (Word32, Word32, Word32) -> Int
+solveNaive (a, b, k) = length [ () | x <- [0..(a-1)], y <- [0..(b-1)], x .&. y < k]
 
+-- Solution which basically counts the number of accepting paths
+-- in a product of three automata on the alphabet {0,1}^2 testing for:
+-- - map fst <= a
+-- - map snd <= b
+-- - map .&. <= k
+
+data State = Success | Failure | Tentative
+           deriving (Eq, Ord)
+
+alphabet = [(x,y) | x <- [False,True], y <- [False,True]]
+
+states = [Success, Failure, Tentative]
+
+nextState Success _ _ = Success
+nextState Failure _ _ = Failure
+nextState Tentative True  False = Failure
+nextState Tentative False True  = Success
+nextState Tentative _     _     = Tentative
+
+states3 = [(x,y,z) | x <- states, y <- states, z <- states]
+
+solve :: (Word32, Word32, Word32) -> Int
+solve (a, b, k) = count 31 Map.! (Tentative, Tentative, Tentative)
+  where count (-1) = Map.insert (Success, Success, Success) 1
+                     $ Map.fromList [(key, 0) | key <- states3]
+        count n = Map.fromList [(key, f key) | key <- states3]
+          where c = count (n-1)
+                f (sa, sb, sk) = sum [ c Map.! next xx | xx <- alphabet]
+                  where next (x, x') = (nextState sa x         (testBit a n),
+                                        nextState sb x'        (testBit b n),
+                                        nextState sk (x && x') (testBit k n))
