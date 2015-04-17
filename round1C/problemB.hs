33e6b9d3c469ba07281ffa95bd9ba5d1d6b11637
Nguyễn Lê Thành Dũng <nltd@nguyentito.eu>
2015-04-18 00:13:11 +0200

    Correct solution for Round 1C Problem B

--- round1C/problemB.hs
+++ round1C/problemB.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE BangPatterns #-}
 
-import Control.Applicative
+import Control.Arrow
 import Control.Monad
-import Data.List
 import Data.Int
-import qualified Data.Set as Set
+import Data.List
+import Data.Maybe
 import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
 import System.IO
 import Debug.Trace
 
@@ -14,27 +15,24 @@ main = do
   forM_ (zip [(1::Int)..] (map solve cases)) $ \(i, sol) ->
     putStrLn $ "Case #" ++ show i ++ ": " ++ show sol
 
-showSol Nothing = "impossible"
-showSol (Just n) = show n
-
 parse :: [String] ->  [[String]]
 parse (numCasesStr:rest) = parse' rest
 
 parse' (_:s:q) = words s : parse' q
 parse' _ = []
 
-data Train = SingleLetter Char
-           | MultiLetter Char (Set.Set Char) Char
-
-preprocess w = case group w of
-  [ww] -> SingleLetter (head ww)
-  w0:w1:ws -> MultiLetter (head w0) (Set.fromList (map head (init (w1:ws)))) (head (last (w1:ws)))
-
+madd, mmult :: Int64 -> Int64 -> Int64
 madd a b = (a + b) `mod` 1000000007
-mmult :: Int64 -> Int64 -> Int64
 mmult a b = (a * b) `mod` 1000000007
-factorial = f 1
+
+fact :: Int64 -> Int64
+fact = f 1
   where f !c !n = if n == 0 then c else f (c `mmult` n) (n-1)
+
+factorial :: (Integral a) => a -> Int64
+factorial = fact . fromIntegral
+
+-- naive solution, performance too bad even for the small input
 
 bruteforce :: [String] -> Int64
 bruteforce ss | length ss > 5 = -1
@@ -43,36 +41,70 @@ bruteforce ss | length ss > 5 = -1
                          s' = nub s
                      in if length s == length s' then trace (show perm) True else False
 
+-- less naive solution
+
+data Train = SingleLetter Char
+           | MultiLetter Char (Set.Set Char) Char
+           deriving (Eq, Show)
+
+preprocess :: String -> Train
+preprocess w = case group w of
+  [ww] -> SingleLetter (head ww)
+  w0:w1:ws -> MultiLetter (head w0)
+                          (Set.fromList . map head . init $ w1:ws)
+                          (head . last $ w1:ws)
+
 solve :: [String] -> Int64
-solve trains = g $ foldM f initial trains'
-  where trains' = zip [0..] . map preprocess $ trains
-        trains'' = Map.fromList trains'
-        initial = (Map.fromList (zip ['a'..'z'] (repeat 0)), Map.empty, Set.empty, Map.empty)
-        f (singles, starts, middles, ends) (ix, train) = case train of
-          SingleLetter c -> Just (Map.adjust (1+) c singles, starts, middles, ends)
-          MultiLetter start curMiddles end
-            | start `Map.member` starts || start `Set.member` middles
-              || end `Set.member` middles || end `Map.member` ends
-              || any (\m -> m `Map.member` starts || m `Set.member` middles
-                            || m `Map.member` ends) (Set.toList curMiddles)
-              -> Nothing
-            | otherwise ->
-              Just (singles, Map.insert start ix starts,
-                    curMiddles `Set.union` middles, Map.insert end ix ends)
-        g Nothing = 0
-        g (Just (singles, starts, _, ends))
-          | let x = map snd . Map.toList $ startends in length (nub x) < length x = 0
-          | otherwise = factorsingle `mmult` factorperm
-          where factorsingle = Map.foldl' mmult 1 . Map.map factorial $ singles
-                startends = Map.intersectionWith (,) starts ends
-                
-                bloup = Map.size (Map.filterWithKey
-                                  (\k x -> x /= 0 &&
-                                           not (k `Map.member` starts) &&
-                                           not (k `Map.member` ends))
-                                  singles)
-                        + length (filter h trains') - Map.size startends
-                factorperm = factorial . fromIntegral $ bloup
-        h (_, SingleLetter _) = False
-        h (_, MultiLetter _ _ _) = True
+solve trains = fromMaybe 0 $ do
+  let trains' = map preprocess trains
+      trains'' = zip [0..] trains'
+
+  let singles = map (head &&& length) . group . sort
+                $ [c | (SingleLetter c) <- trains']
+  middles <- err "middles"
+             $ disjointUnion [c | (MultiLetter _ c _) <- trains']
+  starts <- err "starts"
+            $ fromListUnique [(c, i) | (i, MultiLetter c _ _) <- trains'']
+  ends   <- err "ends"
+            $ fromListUnique [(c, i) | (i, MultiLetter _ _ c) <- trains'']
+
+  mapM_ (err "intermiddle" . guard . null . Set.intersection middles)
+    $ [Map.keysSet starts, Map.keysSet ends, Set.fromList (map fst singles)]
+
+  let startends = Map.intersectionWith (,) starts ends
+      adjacency = Map.fromList . map snd . Map.toList $ startends
+      
+  err "cycle" . guard . not $ cyclic adjacency
+
+  let factorsingle = foldl' mmult 1 [factorial n | (_, n) <- singles]
+      factorperm = factorial bloup
+      bloup = length [() | (MultiLetter c _ _) <- trains',
+                      not (c `Map.member` ends)]
+              + length [() | (c, _) <- singles,
+                        not (c `Map.member` starts),
+                        not (c `Map.member` ends)]
+
+  return $ factorsingle `mmult` factorperm
+
+disjointUnion :: (Ord a) => [Set.Set a] -> Maybe (Set.Set a)
+disjointUnion = foldM f Set.empty
+  where f acc this | not . Set.null $ acc `Set.intersection` this = Nothing
+                   | otherwise = Just $ acc `Set.union` this
+
+fromListUnique :: (Ord k) => [(k, v)] -> Maybe (Map.Map k v)
+fromListUnique = sequenceA
+                 . Map.fromListWith (\_ _ -> Nothing)
+                 . map (second Just)
+
+cyclic :: Map.Map Int Int -> Bool
+cyclic m | Map.null m = False
+         | otherwise = not . Map.null $ iterate sq m !! lg
+  where lg = (+2) . length . takeWhile (/= 0) . iterate (`div` 2) . fst $ Map.findMax m
+        sq mm = Map.mapMaybe (flip Map.lookup mm) mm
+
+err = flip const
+
+-- err :: a -> Maybe b -> Either a b
+-- err e (Just x) = Right x
+-- err e Nothing = Left e
 
